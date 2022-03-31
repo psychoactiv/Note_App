31d5b8ce0761cd13626d540bf2534bdc1fe7a33e
psychoactiv <pritam54majumdar@gmail.com>
2022-03-31 23:22:13 +0530

    refactored-changes

--- src/components/Inputbox/Input.jsx
+++ src/components/Inputbox/Input.jsx
@@ -1,44 +1,31 @@
-import React, { useReducer } from "react";
-import { shouldExpand, initialInpState } from "../../utils/user-inp-reducerfn";
+import React, { useState } from "react";
 import "./input.css";
 
 const InputNote = () => {
-  const [displayInp, dispatchInpVal] = useReducer(
-    shouldExpand,
-    initialInpState
-  );
+  const [displayInp, setDisplayInp] = useState(false);
 
   function HandleSubmit(e) {
     e.preventDefault();
   }
 
   return (
-    <form
-      className="input-box-holder"
-      onSubmit={HandleSubmit}
-      onFocus={() => {
-        dispatchInpVal({ type: "EXPAND", payload: true });
-      }}
-    >
+    <form className="input-box-holder" onSubmit={HandleSubmit}>
       <input
         type="text"
         placeholder="Title"
-        className={`title-text ${
-          displayInp.openStatus ? `block-display` : `title-text`
+        className={`${
+          displayInp ? `block-display` : `title-text`
         } font-wt-semibold`}
-        onBlur={() => dispatchInpVal({ type: "TITLE", payload: false })}
-        onFocus={() => dispatchInpVal({ type: "TITLE", payload: true })}
       />
       <input
         type="text"
         placeholder="Take a note"
-        onBlur={() => dispatchInpVal({ type: "NOTE_TEXT", payload: false })}
-        onFocus={() => dispatchInpVal({ type: "NOTE_TEXT", payload: true })}
+        onFocus={() => setDisplayInp(true)}
       />
       <div className="flex inp-utils-space">
         <button
           className="user-inp-btn-close"
-          onClick={() => dispatchInpVal({ type: "COLAPASE", payload: false })}
+          onClick={() => setDisplayInp(false)}
         >
           close
         </button>
